d714560441f40012ed19820b4cd23ee2ca57acdd
Cindy Kalkomey <cindy@lakesong.net>
2018-02-14 17:27:23 -0600

    Exercise: Counter (round 1)

---
+++ flask_fundamentals/counter/counter.py
@@ -0,0 +1,25 @@
+''' Assignment: Counter
+Build a flask application that counts the number of times the root route ('/') has been viewed. 
+This assignment is to test your understanding of session.
+* Ninja Level 1: Add a +2 button underneath the counter that reloads the page and increments counter by 2. Add another route to handle this functionality.
+* Ninja Level 2: Add a reset button that resets the counter back to 1. Add another route to handle this functionality. '''
+
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret' # you need to set a secret key for security purposes
+
+first = "true"
+@app.route('/')
+def index():
+  if 'cntr' not in session:
+    session['cnty']=0
+  return render_template("index.html")
+
+@app.route('/addtocount', methods=['POST'])
+def count():
+  print "Got Post Info"
+  session['cntr']+=1
+  print session['cntr']
+  return redirect('/')
+
+app.run(debug=True) # run our server
--- flask_fundamentals/form_test/server.py
+++ flask_fundamentals/form_test/server.py
@@ -1,21 +1,40 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'ThisIsSecret' # you need to set a secret key for security purposes
+
 # our index route will handle rendering our form
 @app.route('/')
 def index():
   return render_template("index.html")
+
 # this route will handle our form submission
-# notice how we defined which HTTP methods are allowed by this route
-@app.route('/users', methods=['POST'])
+# notice how we defined which HTTP methods are allowed 
+@app.route('/process', methods=['POST'])
 def create_user():
-   print "Got Post Info"
-   # we'll talk about the following two lines after we learn a little more
-   # about forms
-   name = request.form['name']
-   email = request.form['email']
-   print name, email
-   # redirects back to the '/' route
-   return redirect('/')
-   	 # Here's the line that changed. We're rendering a template from a post route now.
-#    return render_template('success.html')
+  print "Got Post Info"
+  session['action'] = request.form['action']
+  session['first_name'] = request.form['first_name']
+  session['last_name'] = request.form['last_name']
+  session['email'] = request.form['email']
+  session['password'] = request.form['password']
+  print session['action'] 
+
+  if request.form['action'] == 'register':
+    print "It's a registration"
+    return redirect('/show/registration')
+  elif request.form['action'] == 'login':
+    print "It's a login"
+    return redirect('/show/login')
+
+  # redirects back to the '/' route
+  #  return redirect('/')
+
+@app.route('/show/<action>')
+def show_user(action):
+  print action
+  # below is how we would pass session variables to the template
+  # return render_template('user.html', name=session['name'], email=session['email'])
+  # but we don't have to pass them, templates can call them directly
+  return render_template('user.html', action=action)
+
 app.run(debug=True) # run our server
